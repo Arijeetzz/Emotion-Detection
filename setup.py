b112d3ec3a859a5cdd37ca10f245edca9e6d9453
Arijeet <arijeetzzz92@gmail.com>
2025-03-14 08:49:26 +0530

    add cokkiecutter teample

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='To Detect Emotion and to learn DVC , modular code, logging',
+    author='Arijeet Sinha',
+    license='',
+)
